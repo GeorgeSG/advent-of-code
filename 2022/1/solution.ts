93793fa163ccd0a50608c81fac5d5703c81ad5bf
GeorgeSG <georgi@gar.dev>
2022-12-01 15:45:06 +0200

    Refactor tempalte

--- 2022/1/solution.ts
+++ 2022/1/solution.ts
@@ -1,18 +1,14 @@
 import path from 'path';
 import R from 'ramda';
-import { findMax } from '../../utils/arrays';
+import { findMax, sortNums } from '../../utils/arrays';
 import { toI } from '../../utils/numbers';
 import { readFileRaw } from '../../utils/readFile';
 import { timeAndPrint } from '../../utils/timeAndPrint';
 
-// const inputFile = path.resolve(__dirname) + '/demo_input';
-const inputFile = path.resolve(__dirname) + '/input';
+const DEMO = true;
 
 type InputLine = string;
 type Input = InputLine[];
-
-// Input Data
-let inputData: Input = readFileRaw(inputFile).split('\n');
 
 const caloriesPerElf = (input: Input) => R.splitWhenever(R.equals(NaN), input.map(toI)).map(R.sum);
 
@@ -23,8 +19,14 @@ function partA(input: Input) {
 
 // ---- Part B ----
 function partB(input: Input) {
-  return R.sum(caloriesPerElf(input).slice(0, 3));
+  return R.sum(sortNums(caloriesPerElf(input), true).slice(0, 3));
 }
+
+const INPUT_DEMO = path.resolve(__dirname) + '/demo_input';
+const INPUT_REAL = path.resolve(__dirname) + '/input';
+
+// Input Data
+let inputData: Input = readFileRaw(DEMO ? INPUT_DEMO : INPUT_REAL).split('\n');
 
 timeAndPrint(
   () => partA(inputData),
