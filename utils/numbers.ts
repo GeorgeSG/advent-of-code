cd93062262483e2297c9b6151ad31f0f6c3d2647
Georgi Gardev <georgi@gar.dev>
2022-12-13 10:36:05 +0200

    Refactor number compare

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -4,9 +4,5 @@ export const toI = (i) => parseInt(i);
 
 export const numberCompare =
   (direction: SortDirection = SortDirection.ASC) =>
-  (a: number, b: number) => {
-    if (a === b) return 0;
-
-    const multiplier = direction === SortDirection.ASC ? 1 : -1;
-    return a < b ? multiplier * -1 : multiplier * 1;
-  };
+  (a: number, b: number) =>
+    direction === SortDirection.ASC ? Math.sign(a - b) : Math.sign(b - a);
