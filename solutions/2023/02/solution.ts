dacbdd1dff34dd4fe81ef5b139b553ea318a341a
Georgi Gardev <georgi@gar.dev>
2023-12-02 11:03:27 +0200

    Solve 2023/02

---
+++ solutions/2023/02/solution.ts
@@ -0,0 +1,44 @@
+import R from 'ramda';
+import { readFile } from '~utils/core';
+
+type Input = string[];
+
+type RgbArray = [number, number, number];
+
+// Parser
+export function prepareInput(inputFile: string): Input {
+  return readFile(inputFile);
+}
+
+const findValue = (regex: RegExp, line: string) => Number(regex.exec(line)?.[1] ?? 0);
+
+export const findMaxTurns = (game: string) =>
+  game
+    .split(':')[1]
+    .split(';')
+    .reduce(
+      (max, round) => {
+        max[0] = Math.max(max[0], findValue(/(\d+) red/g, round));
+        max[1] = Math.max(max[1], findValue(/(\d+) green/g, round));
+        max[2] = Math.max(max[2], findValue(/(\d+) blue/g, round));
+        return max;
+      },
+      [0, 0, 0]
+    );
+
+// ---- Part A ----
+export function partA(games: Input): number {
+  const GAME_LIMITS: RgbArray = [12, 13, 14];
+
+  return games.reduce((result, gameInput, i) => {
+    const hasFailure = findMaxTurns(gameInput).some(
+      (gameColor, colorIndex) => gameColor > GAME_LIMITS[colorIndex]
+    );
+    return hasFailure ? result : result + i + 1;
+  }, 0);
+}
+
+// ---- Part B ----
+export function partB(games: Input): number {
+  return games.reduce((power, game) => power + R.product(findMaxTurns(game)), 0);
+}
