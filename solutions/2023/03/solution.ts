cbbe6e69c2084f466187c3d038e45b659f5e7e2e
Georgi Gardev <georgi@gar.dev>
2023-12-03 11:41:41 +0200

    Solve 2023/03

---
+++ solutions/2023/03/solution.ts
@@ -0,0 +1,64 @@
+import { product, sum } from 'ramda';
+import { readFile } from '~utils/core';
+import { Point2D, findAdjacentAll } from '~utils/points';
+
+type Input = string[];
+
+// Parser
+export function prepareInput(inputFile: string): Input {
+  return readFile(inputFile);
+}
+
+function allIndexesOf(str: string, char: string) {
+  return str
+    .split('')
+    .reduce<number[]>((indexes, current, i) => (current === char ? [...indexes, i] : indexes), []);
+}
+
+function matchAllNumbers(line: string) {
+  return line.matchAll(/(\d+)/g);
+}
+
+function toNumber(match: RegExpMatchArray): number {
+  return Number(match[0]);
+}
+
+// ---- Part A ----
+export function partA(input: Input): number {
+  const inputMap = input.map((line) => line.split(''));
+
+  const isSymbol = ({ x, y }: Point2D) => !/[0-9.]/.test(inputMap[x][y]);
+
+  const isPart = (numberMatch: RegExpMatchArray, lineIndex: number) =>
+    numberMatch[0]
+      .split('')
+      .some((_, i) =>
+        findAdjacentAll({ x: lineIndex, y: numberMatch.index + i }, inputMap).some(isSymbol)
+      );
+
+  const partNumbersOnLine = (line: string, lineIndex: number) =>
+    [...matchAllNumbers(line)].filter((match) => isPart(match, lineIndex)).map(toNumber);
+
+  return sum(input.flatMap(partNumbersOnLine));
+}
+
+// ---- Part B ----
+export function partB(input: Input): number {
+  const GEAR = '*';
+
+  const adjacentPartsOnLine = (gearIndex: number, line: string) =>
+    [...matchAllNumbers(line)]
+      .filter((match) => match.index - 1 <= gearIndex && match.index + match[0].length >= gearIndex)
+      .map(toNumber);
+
+  const gearRatiosOnLine = (line: string, lineIndex: number) =>
+    allIndexesOf(line, GEAR).map((gearIndex) => {
+      const adjacentParts = [-1, 0, 1].flatMap((lineOffset) =>
+        adjacentPartsOnLine(gearIndex, input[lineIndex + lineOffset])
+      );
+
+      return adjacentParts.length === 2 ? product(adjacentParts) : 0;
+    });
+
+  return sum(input.flatMap(gearRatiosOnLine));
+}
