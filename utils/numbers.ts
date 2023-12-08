6448705606ba0f349f980d9e38bae1f07d4a900b
Georgi Gardev <georgi@gar.dev>
2023-12-08 09:56:55 +0200

    Solve 2023/08

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -6,3 +6,15 @@ export const numberCompare =
   (direction: SortDirection = SortDirection.ASC) =>
   (a: number, b: number) =>
     direction === SortDirection.ASC ? Math.sign(a - b) : Math.sign(b - a);
+
+export function gcd(a: number, b: number): number {
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+export function lcm(a: number, b: number): number {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+export function arrayLcm(numbers: number[]): number {
+  return numbers.reduce(lcm);
+}
