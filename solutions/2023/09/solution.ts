165853da1578d8b8fe0f4a1c5acb228eee45ade8
Georgi Gardev <georgi@gar.dev>
2023-12-09 16:17:07 +0200

    Refactor

--- solutions/2023/09/solution.ts
+++ solutions/2023/09/solution.ts
@@ -1,4 +1,4 @@
-import { head, last, sum } from 'ramda';
+import { head, last, sum, until } from 'ramda';
 import { readFile } from '~utils/core';
 
 type Histogram = number[];
@@ -9,35 +9,33 @@ export function prepareInput(inputFile: string): Input {
   return readFile(inputFile, (line) => line.split(' ').map(Number));
 }
 
-function getHistory(histogram: Histogram) {
-  let currentValues = histogram;
-  let result = [currentValues];
+function getHistory(histogram: Histogram): Histogram[] {
+  let history = [];
 
-  while (currentValues.some((value) => value !== 0)) {
-    currentValues = currentValues.reduce((acc, value, index) => {
-      if (index < currentValues.length - 1) {
-        acc[index] = currentValues[index + 1] - value;
-      }
-      return acc;
-    }, []);
+  until<Histogram, Histogram>(
+    (currentHistogram) => currentHistogram.every((value) => value === 0),
+    (currentHistogram) => {
+      const currentValues = currentHistogram.reduce((acc, value, index) => {
+        if (index < currentHistogram.length - 1) {
+          acc[index] = currentHistogram[index + 1] - value;
+        }
+        return acc;
+      }, []);
+      history.push(currentValues);
+      return currentValues;
+    }
+  )(histogram);
 
-    result.push(currentValues);
-  }
-
-  return result;
+  return [histogram, ...history];
 }
 // ---- Part A ----
 export function partA(input: Input): number {
-  return sum(input.flatMap((histogram) => getHistory(histogram).map(last<number>)));
+  return sum(input.flatMap((histogram) => getHistory(histogram).map<number>(last)));
 }
 
 // ---- Part B ----
 export function partB(input: Input): number {
-  const histories = input.map((histogram) =>
-    getHistory(histogram)
-      .map(head<number>)
-      .reverse()
-  );
+  const histories = input.map((histogram) => getHistory(histogram).map<number>(head).reverse());
 
   return sum(histories.map((history) => history.reduce((acc, value) => value - acc, 0)));
 }
