accdfe612e36533e072e1cc68c0a6774184f38ab
Georgi Gardev <georgi@gar.dev>
2023-12-16 09:08:20 +0200

    Refactor more

--- solutions/2023/16/solution.ts
+++ solutions/2023/16/solution.ts
@@ -13,16 +13,28 @@ export function prepareInput(inputFile: string): Input {
 
 const REFLECTIONS = {
   '/': {
-    [Direction.UP]: Direction.RIGHT,
-    [Direction.DOWN]: Direction.LEFT,
-    [Direction.LEFT]: Direction.DOWN,
-    [Direction.RIGHT]: Direction.UP,
+    [Direction.UP]: [Direction.RIGHT],
+    [Direction.DOWN]: [Direction.LEFT],
+    [Direction.LEFT]: [Direction.DOWN],
+    [Direction.RIGHT]: [Direction.UP],
   },
   '\\': {
-    [Direction.UP]: Direction.LEFT,
-    [Direction.DOWN]: Direction.RIGHT,
-    [Direction.LEFT]: Direction.UP,
-    [Direction.RIGHT]: Direction.DOWN,
+    [Direction.UP]: [Direction.LEFT],
+    [Direction.DOWN]: [Direction.RIGHT],
+    [Direction.LEFT]: [Direction.UP],
+    [Direction.RIGHT]: [Direction.DOWN],
+  },
+  '-': {
+    [Direction.UP]: [Direction.LEFT, Direction.RIGHT],
+    [Direction.DOWN]: [Direction.LEFT, Direction.RIGHT],
+    [Direction.LEFT]: [Direction.LEFT],
+    [Direction.RIGHT]: [Direction.RIGHT],
+  },
+  '|': {
+    [Direction.UP]: [Direction.UP],
+    [Direction.DOWN]: [Direction.DOWN],
+    [Direction.LEFT]: [Direction.UP, Direction.DOWN],
+    [Direction.RIGHT]: [Direction.UP, Direction.DOWN],
   },
 };
 
@@ -43,49 +55,12 @@ function energize(
 
   visited.set(key, [...(visited.get(key) || []), direction]);
 
-  switch (map.get(current)) {
-    case '.': {
-      energize(map, current.move(direction), direction, visited);
-      break;
-    }
-    case '-': {
-      switch (direction) {
-        case Direction.UP:
-        case Direction.DOWN:
-          energize(map, current.move(Direction.LEFT), Direction.LEFT, visited);
-          energize(map, current.move(Direction.RIGHT), Direction.RIGHT, visited);
-          break;
-        case Direction.LEFT:
-        case Direction.RIGHT:
-          energize(map, current.move(direction), direction, visited);
-          break;
-      }
-      break;
-    }
-    case '|': {
-      switch (direction) {
-        case Direction.UP:
-        case Direction.DOWN:
-          energize(map, current.move(direction), direction, visited);
-          break;
-        case Direction.LEFT:
-        case Direction.RIGHT:
-          energize(map, current.move(Direction.UP), Direction.UP, visited);
-          energize(map, current.move(Direction.DOWN), Direction.DOWN, visited);
-          break;
-      }
-      break;
-    }
-    case '/': {
-      direction = REFLECTIONS['/'][direction];
-      energize(map, current.move(direction), direction, visited);
-      break;
-    }
-    case '\\': {
-      direction = REFLECTIONS['\\'][direction];
-      energize(map, current.move(direction), direction, visited);
-      break;
-    }
+  if (map.get(current) === '.') {
+    energize(map, current.move(direction), direction, visited);
+  } else {
+    REFLECTIONS[map.get(current)][direction].forEach((d) =>
+      energize(map, current.move(d), d, visited)
+    );
   }
 }
 
