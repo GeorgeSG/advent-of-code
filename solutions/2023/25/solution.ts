7f7566ae1ed8d985d479b8769ab9da85907277b7
Georgi Gardev <georgi@gar.dev>
2023-12-25 10:56:27 +0200

    Solve 2023/25

---
+++ solutions/2023/25/solution.ts
@@ -0,0 +1,79 @@
+import { RunType } from 'cli/lib/solutionRunner';
+import { readFile } from '~utils/core';
+
+type Input = Record<string, string[]>;
+
+// Parser
+export function prepareInput(inputFile: string): Input {
+  const result: Input = {};
+  readFile(inputFile, (line) => {
+    const [from, to] = line.split(': ');
+    const tos = to.split(' ');
+    result[from] = (result[from] || []).concat(tos);
+  });
+
+  return result;
+}
+
+function printGraphvizDot(input: Input) {
+  console.log('digraph { layout=neato ');
+  console.log(
+    Object.keys(input)
+      .map((key) => `{${key} -> {${input[key].join(' ')}}}`)
+      .join(' ')
+  );
+  console.log('}');
+}
+
+// ---- Part A ----
+export function partA(input: Input, { runType }: { runType: RunType }): number {
+  const removeEdge = (v1: string, v2: string) => {
+    if (input[v1]) {
+      input[v1] = input[v1].filter((x) => x !== v2);
+    }
+    if (input[v2]) {
+      input[v2] = input[v2].filter((x) => x !== v1);
+    }
+  };
+
+  // printGraphvizDot(input);
+
+  if (runType === RunType.REAL) {
+    removeEdge('ldl', 'fpg');
+    removeEdge('hcf', 'lhn');
+    removeEdge('nxk', 'dfk');
+  } else {
+    removeEdge('cmg', 'bvb');
+    removeEdge('pzl', 'hfx');
+    removeEdge('jqt', 'nvd');
+  }
+
+  // Make edges bidirectional
+  const allEdges = new Set<string>();
+
+  Object.keys(input).forEach((from) => {
+    allEdges.add(from);
+    input[from].forEach((to) => {
+      input[to] = [...(input[to] || []), from];
+      allEdges.add(to);
+    });
+  });
+
+  // BFS in a group containing a random vertex
+  const visited = new Set<string>();
+  let current = Object.keys(input)[0];
+  let queue = [current];
+
+  while (queue.length > 0) {
+    visited.add(current);
+    queue = queue.concat(input[current].filter((to) => !visited.has(to)));
+    current = queue.shift();
+  }
+
+  return visited.size * (allEdges.size - visited.size);
+}
+
+// ---- Part B ----
+export function partB(input: Input): number {
+  return 0;
+}
