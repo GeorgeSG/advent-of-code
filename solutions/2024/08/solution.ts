6e547befddb62d6a6774302f10b7bff6c2bee9bf
Georgi Gardev <georgi@gar.dev>
2024-12-08 18:41:49 +0200

    Solve 2024/08

---
+++ solutions/2024/08/solution.ts
@@ -0,0 +1,100 @@
+import { ijMeBro } from '~utils/arrays';
+import { readFile } from '~utils/core';
+import { Point } from '~utils/points';
+
+type Input = {
+  maxX: number;
+  maxY: number;
+  antennasPerFrequency: Record<string, Point[]>;
+};
+
+// Parser
+export function prepareInput(inputFile: string): Input {
+  const lines = readFile(inputFile, (line) => line.split(''));
+
+  const antennas = {};
+
+  ijMeBro(lines, (i, j, char) => {
+    if (char !== '.') {
+      antennas[char] = antennas[char] ? [...antennas[char], new Point(i, j)] : [new Point(i, j)];
+    }
+  });
+
+  return { antennasPerFrequency: antennas, maxX: lines.length, maxY: lines[0].length };
+}
+
+function findAntinode(point: Point, direction: Point, maxX: number, maxY: number): Point | null {
+  const newPoint = point.add(direction);
+  if (newPoint.x >= 0 && newPoint.y >= 0 && newPoint.x < maxX && newPoint.y < maxY) {
+    return newPoint;
+  }
+
+  return null;
+}
+
+function findAntinodes(p1: Point, p2: Point, maxX: number, maxY: number): Set<string> {
+  return new Set<string>(
+    [
+      findAntinode(p1, p1.subtract(p2), maxX, maxY)?.toKey(),
+      findAntinode(p2, p2.subtract(p1), maxX, maxY)?.toKey(),
+    ].filter(Boolean)
+  );
+}
+
+function findAntinodesInDirection(
+  start: Point,
+  diff: Point,
+  maxX: number,
+  maxY: number
+): Set<string> {
+  const anitnodes = new Set<string>();
+  let point = start;
+
+  while (point !== null) {
+    anitnodes.add(point.toKey());
+    point = findAntinode(point, diff, maxX, maxY);
+  }
+
+  return anitnodes;
+}
+
+function findAllAntinodes(p1: Point, p2: Point, maxX: number, maxY: number): Set<string> {
+  return new Set([
+    ...findAntinodesInDirection(p1, p1.subtract(p2), maxX, maxY),
+    ...findAntinodesInDirection(p2, p2.subtract(p1), maxX, maxY),
+  ]);
+}
+
+// ---- Part A ----
+export function partA({ antennasPerFrequency, maxX, maxY }: Input): number {
+  const anitnodes = new Set();
+
+  Object.keys(antennasPerFrequency).forEach((antenna) => {
+    const points = antennasPerFrequency[antenna];
+    for (let i = 0; i < points.length; i++) {
+      for (let j = i + 1; j < points.length; j++) {
+        const newAntinodes = findAntinodes(points[i], points[j], maxX, maxY);
+        newAntinodes.forEach((node) => anitnodes.add(node));
+      }
+    }
+  });
+
+  return anitnodes.size;
+}
+
+// ---- Part B ----
+export function partB({ antennasPerFrequency, maxX, maxY }: Input): number {
+  const anitnodes = new Set();
+
+  Object.keys(antennasPerFrequency).forEach((antenna) => {
+    const points = antennasPerFrequency[antenna];
+    for (let i = 0; i < points.length; i++) {
+      for (let j = i + 1; j < points.length; j++) {
+        const newAntinodes = findAllAntinodes(points[i], points[j], maxX, maxY);
+        newAntinodes.forEach((node) => anitnodes.add(node));
+      }
+    }
+  });
+
+  return anitnodes.size;
+}
