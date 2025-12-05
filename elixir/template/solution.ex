94a902cdd78baac31c4f149f4f1db1877c31178a
Georgi Gardev <georgi@gar.dev>
2025-12-05 12:14:35 +0100

    Update template

---
+++ elixir/template/solution.ex
@@ -0,0 +1,13 @@
+defmodule AdventOfCode.Solutions2025.Task00 do
+  def prepare_input(input) do
+    input
+  end
+
+  def part_a(input) do
+    input
+  end
+
+  def part_b(input) do
+    input
+  end
+end
